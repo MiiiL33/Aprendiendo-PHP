3b49e0eed6535fc7942483f75b24df02be929174
MiiiL33 <miiil33@hotmail.com>
2021-01-11 15:58:27 -0300

    Initial commit

---
+++ 02-imprimiendo/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE HTML>
+<html lang="es">
+    <head>
+        <meta charset="utf-8"/>
+        <title>Imprimir en pantalla</title>
+    </head>
+    <body>
+        <h1>Master en PHP</h1>
+        <?php
+            //titular de la sección
+            echo "<h3>Listado de videojuegos:</h3>";
+            /*Comentario multilinea
+            en PHP*/
+            echo "<u1>"
+                . "<li>GTA: San Andreas</li>"
+                . "<li>Super Mario Bros.</li>"
+                . "<li>DOOM Eternal</li>"
+                . "</u1>";
+            echo "<br>Probando Probando</br>";
+            //parrafo explicativo
+            echo '<p>Esta es toda'.' - '.'lista de juegos</p>';
+        ?>
+    </body>
+</html>
